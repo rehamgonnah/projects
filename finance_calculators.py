1180d8f3f3050c6a3d516fca30c47b078b5e40af
rehamgonnah <154753356+rehamgonnah@users.noreply.github.com>
2024-03-09 10:24:39 +0000

    Update finance_calculators.py

--- finance_calculators.py
+++ finance_calculators.py
@@ -1,47 +1,46 @@
 import math
 
-# output shown to the user as the program runs
-# asks the user to input what they want to calculate
+# Output options for the user
 print("investment - to calculate the amount of interest you'll earn on your investment")
 print("bond - to calculate the amount you'll have to pay on a home loan")
 
+# Prompt the user to select a calculation option
 user_input = input("Enter either 'investment' or 'bond' from the menu above to proceed:")
 
-# unify the inputs regardless of how the user inputs their choice of calculation
-user_input = user_input.lower() # changes all input to lower case 
+# Normalize the user input to lowercase
+user_input = user_input.lower()
 
-# if-elif-else statement based on the user's choice of calculator: 'investment' or 'bond'
+# Branch based on user's choice of calculation: 'investment' or 'bond'
 if user_input == "investment":
 
-    # ask user to input values to calculate interest if 'investment' calculator chosen
+    # Gather necessary inputs for investment calculation
     amount = int(input("Please enter the amount of money that you are depositing"))
     rate = int(input("Please enter the interest rate as a percentage without '%'"))
     rate = rate / 100
     time = int(input("Please enter the number of years you plan on investing"))
     interest = input("Please enter either 'simple' or 'compound'")
 
-    # nested if-elif-else statement if user chooses 'investment' calculator to calculate the interest based on type: 'simple' or 'compound'
+    # Calculate interest based on user's choice: 'simple' or 'compound'
     if interest == "simple":
-        simple_interest = amount * (1 + (rate * time)) # calculates simple interest
-        print("The amount you will get back is: " + str(simple_interest)) # prints value calculated
+        simple_interest = amount * (1 + (rate * time))
+        print("The amount you will get back is: " + str(simple_interest))
     elif interest == "compound":
-        compound_interest = amount * math.pow((1+rate),time) # # calculates compound interest by calling function from math package
-        print("The amount you will get back is: " + str(compound_interest)) # prints value calculated
+        compound_interest = amount * math.pow((1+rate),time)
+        print("The amount you will get back is: " + str(compound_interest))
     else:
-        print("Please enter a valid input") # output if neither 'simple' or 'compound' was inputted
+        print("Please enter a valid input")
 
 elif user_input == "bond": 
-    # ask user to input values to calculate repayment if 'bond' calculator chosen
+
+    # Gather necessary inputs for bond repayment calculation
     house_value = int(input("Please enter the present value of the house as a number only"))
     interest_rate = int(input("Please enter the interest rate as a number only"))
     interest_rate = (interest_rate / 100) / 12
     months = int(input("Please enter the number of months you plan to take to repay the bond"))
-    repayment = (interest_rate * house_value) / (1 - (1 + interest_rate) ** (-months)) # calculates repayment 
+
+    # Calculate monthly repayment for the bond
+    repayment = (interest_rate * house_value) / (1 - (1 + interest_rate) ** (-months))
     print("The amount of money you will have to repay each month is: " + str(repayment))
 
 else:
     print("Please enter a valid input")
-
-
-
-
